ced611ef29a91b816742d7767f8d5532d2513cc1
iamkennytso <iamkennytso@gmail.com>
2022-10-13 18:51:07 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import hardcodeData from './hardcode.json';
+
+const formatPoints = num => (Math.round(num * 100) / 100).toFixed(2)
+
+const { team1Name, team2Name, teamIdToNames, teamOneIds, teamTwoIds} = hardcodeData
+
+const teamOneTotalPoints = formatPoints(data.teamOnePoints.total)
+const teamTwoTotalPoints = formatPoints(data.teamTwoPoints.total)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  return <div className='mainContainer'>
+    <div className='leftContainer halfContainer'>
+      <Typography variant="h2">{team1Name}</Typography>
+
+      {/* Individual Scores */}
+      {teamOneIds.sort((a,b) => data[teamIdToNames[a]].total - data[teamIdToNames[b]].total).map(id => {
+        const teamName = teamIdToNames[id]
+        return <Accordion className='accordianContainer' key={id}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <div className='fullWidth'>
+              <div>
+                {teamName}
+              </div>
+              <div className='rightTextAlign'>
+                {formatPoints(data[teamName].total)}
+              </div>
+            </div>
+          </AccordionSummary>
+          <AccordionDetails>
+            {data[teamName].scores.map((score, week) => score ? <div key={week} className='flexRow'>
+              <div className='oneOfTwoSections'>
+                Wk {week + 1}:
+              </div>
+              <div className='oneOfTwoSections rightTextAlign'>
+                {formatPoints(score)}
+              </div>
+            </div> : null)}
+          </AccordionDetails>
+        </Accordion>
+      })}
+
+      {/* Combined Totals */}
+      <Accordion className='accordianContainer boldFont'>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
         >
-          Learn React
-        </a>
-      </header>
+          <div className='boldFont fullWidth'><div>Total: </div><div className='rightTextAlign'>{teamOneTotalPoints}</div></div>
+        </AccordionSummary>
+        <AccordionDetails>
+          {data.teamOnePoints.scores.map((score, week) => score ? <div key={week} className='flexRow'>
+            <div className='oneOfTwoSections'>
+              Wk {week + 1}:
+            </div>
+            <div className='oneOfTwoSections rightTextAlign'>
+              {formatPoints(score)}
+            </div>
+          </div> : null)}
+        </AccordionDetails>
+      </Accordion>
+
+      {/* Team Diffs */}
+      <Accordion className='accordianContainer boldFont'>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <div className={`fullWidth ${teamOneTotalPoints - teamTwoTotalPoints < 1 ? 'redFont' : 'greenFont'}`}>
+            <div>Diff: </div>
+            <div className='rightTextAlign'>{formatPoints(teamOneTotalPoints - teamTwoTotalPoints)}</div>
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          {data.teamOnePoints.scores.map((score, week) => score ? <div key={week}>
+            <div className='fullWidth centerTextAlign'>Week {week + 1}</div>
+            <div className='fullWidth'>
+              <div className={`${score - data.teamTwoPoints.scores[week] < 1 ? 'redFont' : 'greenFont'}`}>{formatPoints(score - data.teamTwoPoints.scores[week])}</div>
+              <div className={`rightTextAlign ${data.teamOnePoints.rollingDiff[week] < 1 ? 'redFont' : 'greenFont'} `}>{formatPoints(data.teamOnePoints.rollingDiff[week])}</div>
+            </div>
+          </div> : null)}
+        </AccordionDetails>
+      </Accordion>
     </div>
-  );
+
+
+    {/* Right Side */}
+
+    <div className='rightContainer halfContainer'>
+      <Typography variant="h2">{team2Name}</Typography>
+      {/* Individual Scores */}
+      {teamTwoIds.sort((a,b) => data[teamIdToNames[a]].total - data[teamIdToNames[b]].total).map(id => {
+        const teamName = teamIdToNames[id]
+        return <Accordion className='accordianContainer' key={id}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <div className='fullWidth'>
+              <div>
+                {teamName}
+              </div>
+              <div className='rightTextAlign'>
+                {formatPoints(data[teamName].total)}
+              </div>
+            </div>
+          </AccordionSummary>
+          <AccordionDetails>
+            {data[teamName].scores.map((score, week) => score ? <div key={week} className='flexRow'>
+              <div className='oneOfTwoSections'>
+                Wk {week + 1}:
+              </div>
+              <div className='oneOfTwoSections rightTextAlign'>
+                {formatPoints(score)}
+              </div>
+            </div> : null)}
+          </AccordionDetails>
+        </Accordion>
+      })}
+
+      {/* Combined Totals */}
+      <Accordion className='accordianContainer boldFont'>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <div className='fullWidth boldFont'><div>Total: </div><div className='rightTextAlign'>{teamTwoTotalPoints}</div></div>
+        </AccordionSummary>
+        <AccordionDetails>
+          {data.teamTwoPoints.scores.map((score, week) => score ? <div key={week} className='flexRow'>
+            <div className='oneOfTwoSections'>
+              Wk {week + 1}:
+            </div>
+            <div className='oneOfTwoSections rightTextAlign'>
+              {formatPoints(score)}
+            </div>
+          </div> : null)}
+        </AccordionDetails>
+      </Accordion>
+
+      {/* Team Diffs */}
+      <Accordion className='accordianContainer boldFont'>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <div className={`fullWidth ${teamTwoTotalPoints - teamOneTotalPoints < 1 ? 'redFont' : 'greenFont'}`}>
+            <div>Diff: </div>
+            <div className='rightTextAlign'>{formatPoints(teamTwoTotalPoints - teamOneTotalPoints)}</div>
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          {data.teamTwoPoints.scores.map((score, week) => score ? <div key={week}>
+            <div className='fullWidth centerTextAlign'>Week {week + 1}</div>
+            <div className='fullWidth'>
+              <div className={`${score - data.teamOnePoints.scores[week] < 1 ? 'redFont' : 'greenFont'}`}>{formatPoints(score - data.teamOnePoints.scores[week])}</div>
+              <div className={`rightTextAlign ${data.teamTwoPoints.rollingDiff[week] < 1 ? 'redFont' : 'greenFont'} `}>{formatPoints(data.teamTwoPoints.rollingDiff[week])}</div>
+            </div>
+          </div> : null)}
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  </div>
 }
 
 export default App;
---
+++ src/backend.js
@@ -0,0 +1,78 @@
+var axios = require('axios');
+const fs = require('fs');
+const { teamIds, teamOneIds, teamIdToNames } = require('./hardcode');
+require('dotenv').config()
+
+const currentYear = 2022
+
+var config = {
+  method: 'get',
+  url: `https://fantasy.espn.com/apis/v3/games/ffl/seasons/${currentYear}/segments/0/leagues/${process.env.LEAGUE_ID}?view=mMatchup`,
+  // cookie needed if private league
+  headers: {
+    'Cookie': `SWID={${process.env.SWID}}; _dcf=1; espn_s2=${process.env.ESPN_S2}; region=ccpa`
+  }
+};
+
+const teamsData = teamIds.reduce((accu, team) => {
+  accu[teamIdToNames[team]] = { scores: [], teamOne: teamOneIds.includes(team), total: 0 }
+  return accu
+}, {})
+const teamOnePoints = { scores: [], total: 0 }
+const teamTwoPoints = { scores: [], total: 0 }
+
+axios(config)
+  .then(function (response) {
+    for (let game of response.data.schedule) {
+      const { away, home, matchupPeriodId } = game;
+      const { teamId: awayId, totalPoints: awayPoints } = away
+      const { teamId: homeId, totalPoints: homePoints } = home
+      if (awayPoints) {
+        teamsData[teamIdToNames[awayId]].scores[matchupPeriodId - 1] = awayPoints
+        teamsData[teamIdToNames[awayId]].total += awayPoints
+      }
+      if (homePoints) {
+        teamsData[teamIdToNames[homeId]].scores[matchupPeriodId - 1] = homePoints
+        teamsData[teamIdToNames[homeId]].total += homePoints
+      }
+
+      if (teamsData[teamIdToNames[awayId]].teamOne) {
+        teamOnePoints.scores[matchupPeriodId - 1]
+          ? teamOnePoints.scores[matchupPeriodId - 1] += awayPoints
+          : teamOnePoints.scores[matchupPeriodId - 1] = awayPoints
+          teamOnePoints.total += awayPoints
+      } else {
+        teamTwoPoints.scores[matchupPeriodId - 1]
+          ? teamTwoPoints.scores[matchupPeriodId - 1] += awayPoints
+          : teamTwoPoints.scores[matchupPeriodId - 1] = awayPoints
+        teamTwoPoints.total += awayPoints
+      }
+
+      if (teamsData[teamIdToNames[homeId]].teamOne) {
+        teamOnePoints.scores[matchupPeriodId - 1]
+          ? teamOnePoints.scores[matchupPeriodId - 1] += homePoints
+          : teamOnePoints.scores[matchupPeriodId - 1] = homePoints
+        teamOnePoints.total += homePoints
+      } else {
+        teamTwoPoints.scores[matchupPeriodId - 1]
+          ? teamTwoPoints.scores[matchupPeriodId - 1] += homePoints
+          : teamTwoPoints.scores[matchupPeriodId - 1] = homePoints
+        teamTwoPoints.total += homePoints
+      }
+    }
+
+    let hold = 0
+    teamOnePoints.rollingDiff = teamOnePoints.scores.map((week, idx) => {
+      const prevScore = hold
+      const rollingDiff = prevScore + week - teamTwoPoints.scores[idx]
+      hold = rollingDiff
+      return rollingDiff
+    })
+    
+    teamTwoPoints.rollingDiff = teamOnePoints.rollingDiff.map(score => score * -1)
+
+    fs.writeFileSync('src/data.json', JSON.stringify({ ...teamsData, teamOnePoints, teamTwoPoints }));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
