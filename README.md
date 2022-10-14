7696b09c2c04944dfa667c208728881e78a4b152
iamkennytso <iamkennytso@gmail.com>
2022-10-14 00:07:38 -0400

    refactor

--- README.md
+++ README.md
@@ -1,70 +1,12 @@
-# Getting Started with Create React App
+This app is for those who play Fantasy Football on ESPN and want to have two sides compete against each other.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Be sure to have a .env file with at least 1 variables:
+`LEAGUE_ID` is your league ID
+`SWID` and `ESPN_S2` are necessary if your league is private. You can get these via your cookies in an active session with ESPN.
 
-## Available Scripts
+In addition, you'll need to set up some of the `hardcode.json` to your league specifically for this to work properly.
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+The original personal use project is hosted on firebase, which is why the `npm run update` will
+1) run `backend.js` to get the latest data and save it as `data.json`
+2) build the react app 
+3) deploy to firebase
--- src/App.js
+++ src/App.js
@@ -9,159 +9,98 @@ import hardcodeData from './hardcode.json';
 
 const formatPoints = num => (Math.round(num * 100) / 100).toFixed(2)
 
-const { team1Name, team2Name, teamIdToNames, teamOneIds, teamTwoIds} = hardcodeData
+const { team1Name, team2Name, teamIdToNames, teamOneIds, teamTwoIds } = hardcodeData;
 
 const teamOneTotalPoints = formatPoints(data.teamOnePoints.total)
 const teamTwoTotalPoints = formatPoints(data.teamTwoPoints.total)
 
+const TeamPane = ({sideName, sideOneOrTwo, sideTeamIds}) => {
+  const sideTotalPoints = sideOneOrTwo === 'one' ? teamOneTotalPoints : teamTwoTotalPoints
+  const opposingTotalPoints = sideOneOrTwo === 'one' ? teamTwoTotalPoints : teamOneTotalPoints
+  const sidePoints = sideOneOrTwo === 'one' ? 'teamOnePoints' : 'teamTwoPoints'
+  const opposingSidePoints = sideOneOrTwo === 'one' ? 'teamTwoPoints' : 'teamOnePoints'
+
+  return <>
+    <Typography variant="h2">{sideName}</Typography>
+
+    {/* Individual Scores */}
+    {sideTeamIds.sort((a, b) => data[teamIdToNames[a]].total - data[teamIdToNames[b]].total).map(id => {
+      const teamName = teamIdToNames[id]
+      return <Accordion className='accordianContainer' key={id}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <div className='fullWidth'>
+            <div>
+              {teamName}
+            </div>
+            <div className='rightTextAlign'>
+              {formatPoints(data[teamName].total)}
+            </div>
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          {data[teamName].scores.map((score, week) => score ? <div key={week} className='flexRow'>
+            <div className='oneOfTwoSections'>
+              Wk {week + 1}:
+            </div>
+            <div className='oneOfTwoSections rightTextAlign'>
+              {formatPoints(score)}
+            </div>
+          </div> : null)}
+        </AccordionDetails>
+      </Accordion>
+    })}
+
+    {/* Combined Totals */}
+    <Accordion className='accordianContainer boldFont'>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <div className='boldFont fullWidth'><div>Total: </div><div className='rightTextAlign'>{sideTotalPoints}</div></div>
+      </AccordionSummary>
+      <AccordionDetails>
+        {data[sidePoints].scores.map((score, week) => score ? <div key={week} className='flexRow'>
+          <div className='oneOfTwoSections'>
+            Wk {week + 1}:
+          </div>
+          <div className='oneOfTwoSections rightTextAlign'>
+            {formatPoints(score)}
+          </div>
+        </div> : null)}
+      </AccordionDetails>
+    </Accordion>
+
+    {/* Team Diffs */}
+    <Accordion className='accordianContainer boldFont'>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <div className={`fullWidth ${sideTotalPoints - opposingTotalPoints < 1 ? 'redFont' : 'greenFont'}`}>
+          <div>Diff: </div>
+          <div className='rightTextAlign'>{formatPoints(sideTotalPoints - opposingTotalPoints)}</div>
+        </div>
+      </AccordionSummary>
+      <AccordionDetails>
+        {data[sidePoints].scores.map((score, week) => score ? <div key={week}>
+          <div className='fullWidth centerTextAlign'>Week {week + 1}</div>
+          <div className='fullWidth'>
+            <div className={`${score - data[opposingSidePoints].scores[week] < 1 ? 'redFont' : 'greenFont'}`}>{formatPoints(score - data[opposingSidePoints].scores[week])}</div>
+            <div className={`rightTextAlign ${data[sidePoints].rollingDiff[week] < 1 ? 'redFont' : 'greenFont'} `}>{formatPoints(data[sidePoints].rollingDiff[week])}</div>
+          </div>
+        </div> : null)}
+      </AccordionDetails>
+    </Accordion>
+  </>
+}
+
 function App() {
   return <div className='mainContainer'>
     <div className='leftContainer halfContainer'>
-      <Typography variant="h2">{team1Name}</Typography>
-
-      {/* Individual Scores */}
-      {teamOneIds.sort((a,b) => data[teamIdToNames[a]].total - data[teamIdToNames[b]].total).map(id => {
-        const teamName = teamIdToNames[id]
-        return <Accordion className='accordianContainer' key={id}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-          >
-            <div className='fullWidth'>
-              <div>
-                {teamName}
-              </div>
-              <div className='rightTextAlign'>
-                {formatPoints(data[teamName].total)}
-              </div>
-            </div>
-          </AccordionSummary>
-          <AccordionDetails>
-            {data[teamName].scores.map((score, week) => score ? <div key={week} className='flexRow'>
-              <div className='oneOfTwoSections'>
-                Wk {week + 1}:
-              </div>
-              <div className='oneOfTwoSections rightTextAlign'>
-                {formatPoints(score)}
-              </div>
-            </div> : null)}
-          </AccordionDetails>
-        </Accordion>
-      })}
-
-      {/* Combined Totals */}
-      <Accordion className='accordianContainer boldFont'>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-        >
-          <div className='boldFont fullWidth'><div>Total: </div><div className='rightTextAlign'>{teamOneTotalPoints}</div></div>
-        </AccordionSummary>
-        <AccordionDetails>
-          {data.teamOnePoints.scores.map((score, week) => score ? <div key={week} className='flexRow'>
-            <div className='oneOfTwoSections'>
-              Wk {week + 1}:
-            </div>
-            <div className='oneOfTwoSections rightTextAlign'>
-              {formatPoints(score)}
-            </div>
-          </div> : null)}
-        </AccordionDetails>
-      </Accordion>
-
-      {/* Team Diffs */}
-      <Accordion className='accordianContainer boldFont'>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-        >
-          <div className={`fullWidth ${teamOneTotalPoints - teamTwoTotalPoints < 1 ? 'redFont' : 'greenFont'}`}>
-            <div>Diff: </div>
-            <div className='rightTextAlign'>{formatPoints(teamOneTotalPoints - teamTwoTotalPoints)}</div>
-          </div>
-        </AccordionSummary>
-        <AccordionDetails>
-          {data.teamOnePoints.scores.map((score, week) => score ? <div key={week}>
-            <div className='fullWidth centerTextAlign'>Week {week + 1}</div>
-            <div className='fullWidth'>
-              <div className={`${score - data.teamTwoPoints.scores[week] < 1 ? 'redFont' : 'greenFont'}`}>{formatPoints(score - data.teamTwoPoints.scores[week])}</div>
-              <div className={`rightTextAlign ${data.teamOnePoints.rollingDiff[week] < 1 ? 'redFont' : 'greenFont'} `}>{formatPoints(data.teamOnePoints.rollingDiff[week])}</div>
-            </div>
-          </div> : null)}
-        </AccordionDetails>
-      </Accordion>
+      <TeamPane sideName={team1Name} sideOneOrTwo='one' sideTeamIds={teamOneIds} />
     </div>
-
-
-    {/* Right Side */}
-
     <div className='rightContainer halfContainer'>
-      <Typography variant="h2">{team2Name}</Typography>
-      {/* Individual Scores */}
-      {teamTwoIds.sort((a,b) => data[teamIdToNames[a]].total - data[teamIdToNames[b]].total).map(id => {
-        const teamName = teamIdToNames[id]
-        return <Accordion className='accordianContainer' key={id}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-          >
-            <div className='fullWidth'>
-              <div>
-                {teamName}
-              </div>
-              <div className='rightTextAlign'>
-                {formatPoints(data[teamName].total)}
-              </div>
-            </div>
-          </AccordionSummary>
-          <AccordionDetails>
-            {data[teamName].scores.map((score, week) => score ? <div key={week} className='flexRow'>
-              <div className='oneOfTwoSections'>
-                Wk {week + 1}:
-              </div>
-              <div className='oneOfTwoSections rightTextAlign'>
-                {formatPoints(score)}
-              </div>
-            </div> : null)}
-          </AccordionDetails>
-        </Accordion>
-      })}
-
-      {/* Combined Totals */}
-      <Accordion className='accordianContainer boldFont'>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-        >
-          <div className='fullWidth boldFont'><div>Total: </div><div className='rightTextAlign'>{teamTwoTotalPoints}</div></div>
-        </AccordionSummary>
-        <AccordionDetails>
-          {data.teamTwoPoints.scores.map((score, week) => score ? <div key={week} className='flexRow'>
-            <div className='oneOfTwoSections'>
-              Wk {week + 1}:
-            </div>
-            <div className='oneOfTwoSections rightTextAlign'>
-              {formatPoints(score)}
-            </div>
-          </div> : null)}
-        </AccordionDetails>
-      </Accordion>
-
-      {/* Team Diffs */}
-      <Accordion className='accordianContainer boldFont'>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-        >
-          <div className={`fullWidth ${teamTwoTotalPoints - teamOneTotalPoints < 1 ? 'redFont' : 'greenFont'}`}>
-            <div>Diff: </div>
-            <div className='rightTextAlign'>{formatPoints(teamTwoTotalPoints - teamOneTotalPoints)}</div>
-          </div>
-        </AccordionSummary>
-        <AccordionDetails>
-          {data.teamTwoPoints.scores.map((score, week) => score ? <div key={week}>
-            <div className='fullWidth centerTextAlign'>Week {week + 1}</div>
-            <div className='fullWidth'>
-              <div className={`${score - data.teamOnePoints.scores[week] < 1 ? 'redFont' : 'greenFont'}`}>{formatPoints(score - data.teamOnePoints.scores[week])}</div>
-              <div className={`rightTextAlign ${data.teamTwoPoints.rollingDiff[week] < 1 ? 'redFont' : 'greenFont'} `}>{formatPoints(data.teamTwoPoints.rollingDiff[week])}</div>
-            </div>
-          </div> : null)}
-        </AccordionDetails>
-      </Accordion>
+      <TeamPane sideName={team2Name} sideOneOrTwo='two' sideTeamIds={teamTwoIds} />
     </div>
   </div>
 }
